dc939bb88a5e66241a393a0001f113f78491bac4
nohjunh <nsh042777@naver.com>
2023-03-04 18:51:30 +0900

    modifier

--- IntroToCompose/app/src/main/java/com/nohjunh/introtocompose/MainActivity.kt
+++ IntroToCompose/app/src/main/java/com/nohjunh/introtocompose/MainActivity.kt
@@ -3,14 +3,14 @@ package com.nohjunh.introtocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.nohjunh.introtocompose.ui.theme.IntroToComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +21,9 @@ class MainActivity : ComponentActivity() {
             IntroToComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxHeight()
+                        .fillMaxWidth()
+                        .padding(all = 59.dp),
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
