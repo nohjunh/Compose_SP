2e2a6e55378f2bc212ea01ee77f8cd8daf229870
nohjunh <nsh042777@naver.com>
2023-03-05 15:12:29 +0900

    TopHeader, InputTextField

--- TipApp/app/src/main/java/com/nohjunh/tipapp/MainActivity.kt
+++ TipApp/app/src/main/java/com/nohjunh/tipapp/MainActivity.kt
@@ -1,46 +1,56 @@
 package com.nohjunh.tipapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.nohjunh.tipapp.components.InputField
 import com.nohjunh.tipapp.ui.theme.TipAppTheme
 
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Text(text = "Hello Again")
+                //TopHeader()
+                MainContent()
             }
         }
     }
 }
 
 @Composable
-// : 타입형식을 @Composable로 하는 것은
-// 컴파일러에게 컴포저블로 예상이 된다는 것을 알려주기 위함.
+// (content: @Composable () -> Unit) 타입 형식을 @Composable로
+// 하는 것은 컴파일러에게 컴포저블로 예상이 된다는 것을 알려주기 위함.
 // 아래 예시처럼, content에 전달하는 것을 Composable로 넣을 수 있다.
 fun MyApp(content: @Composable () -> Unit) {
     // Theme도 Container
     TipAppTheme {
         // A surface container using the 'background' color from the theme
          Surface(
-            modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colors.background
         ) {
             content()
@@ -48,28 +58,93 @@ fun MyApp(content: @Composable () -> Unit) {
     }
 }
 
-@Preview
+//@Preview
 @Composable
-fun TopHeader() {
+fun TopHeader(totalPerPerson: Double = 134.0) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
             .height(150.dp)
-            .clip(shape = CircleShape.copy(all = CornerSize(12.dp)))
+            .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
             //.clip(shape = RoundedCornerShape(corner = CornerSize(12.dp)))
+        color = Color(0xFFE9D7F7)
     ) {
-        Column() {
-
+        Column(modifier = Modifier
+            .padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(text = "Total Per Person",
+                style = MaterialTheme.typography.h5)
+            Text(text = "$$total",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.ExtraBold)
         }
     }
 }
 
-@Preview(showBackground = true)
+/*
+BillForm 함수의 onValChange 매개변수에 BillForm() { billAmt -> ... }
+로 전달된 람다식이 대입된다.
+이 때, 람다식의 매개변수 billAmt가 onValChange 함수의 매개변수로 전달되어
+해당 함수에서 사용된다. 따라서, BillForm() 함수 안에서
+onValChange 함수가 호출되면, 람다식에서 정의된
+Log.d("AMT", "MainContent: $billAmt") 코드가 실행됨.
+ */
+@ExperimentalComposeUiApi
+@Preview
+@Composable
+fun MainContent() {
+    BillForm() { billAmt -> // billAmt = bill amount
+        Log.d("AMT", "MainContent: $billAmt.toInt()")
+    }
+}
+
+@ExperimentalComposeUiApi
+@Composable
+fun BillForm(
+    modifier: Modifier = Modifier,
+    onValChange: (String) -> Unit = {}
+) {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Surface(
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
+    ) {
+        Column() {
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    onValChange(totalBillState.value.trim())
+
+                    keyboardController?.hide()
+                }
+            )
+        }
+    }
+}
+
+//@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TipAppTheme {
         MyApp {
-            Text("Hello Again")
+            TopHeader()
         }
     }
 }
