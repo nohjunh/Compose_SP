ea7e7b8c1c302ebf936e0be72482ba15bede87ed
nohjunh <nsh042777@naver.com>
2023-03-05 17:04:25 +0900

    Slider Composable + TopHeader

--- TipApp/app/src/main/java/com/nohjunh/tipapp/MainActivity.kt
+++ TipApp/app/src/main/java/com/nohjunh/tipapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Slider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -24,7 +25,6 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,6 +32,7 @@ import androidx.compose.ui.unit.dp
 import com.nohjunh.tipapp.components.InputField
 import com.nohjunh.tipapp.ui.theme.TipAppTheme
 import com.nohjunh.tipapp.widgets.RoundIconButton
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 @ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
@@ -68,6 +69,7 @@ fun TopHeader(totalPerPerson: Double = 134.0) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(15.dp)
             .height(150.dp)
             .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
             //.clip(shape = RoundedCornerShape(corner = CornerSize(12.dp)))
@@ -100,8 +102,13 @@ Log.d("AMT", "MainContent: $billAmt") 코드가 실행됨.
 @Preview
 @Composable
 fun MainContent() {
-    BillForm() { billAmt -> // billAmt = bill amount
-        Log.d("AMT", "MainContent: $billAmt.toInt()")
+    Column(
+        modifier = Modifier
+            .padding(all = 12.dp)
+    ) {
+        BillForm() { billAmt -> // billAmt = bill amount
+            Log.d("AMT", "MainContent: $billAmt")
+        }
     }
 }
 
@@ -119,6 +126,11 @@ fun BillForm(
         totalBillState.value.trim().isNotEmpty()
     }
     val keyboardController = LocalSoftwareKeyboardController.current
+
+    val sliderPositionState = remember {
+        mutableStateOf(0f)
+    }
+    TopHeader()
 
     Surface(
         modifier = Modifier
@@ -145,7 +157,7 @@ fun BillForm(
                     keyboardController?.hide()
                 }
             )
-            if (validState) {
+            //if (validState) {
                 Row(
                     modifier = Modifier
                             .padding(3.dp),
@@ -179,11 +191,43 @@ fun BillForm(
                         )
                     }
                 }
-            }else {
-                Box() {
-
+                // Tip Row
+                Row(
+                    modifier = Modifier
+                        .padding(horizontal = 3.dp, vertical = 12.dp)
+                ) {
+                    Text(
+                        text = "Tip",
+                        modifier = Modifier.align(alignment = Alignment.CenterVertically)
+                    )
+                    Spacer(modifier = Modifier.width(200.dp))
+                    Text(
+                        text = "$33.00",
+                        modifier = Modifier.align(alignment = Alignment.CenterVertically)
+                    )
                 }
-            }
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text= "33%")
+                    Spacer(modifier = Modifier.height(14.dp))
+                    Slider(
+                        value = sliderPositionState.value,
+                        onValueChange = { newVal ->
+                            sliderPositionState.value = newVal
+                            Log.d("slider", "BillForm : $newVal")
+                        },
+                        modifier = Modifier
+                            .padding(start = 16.dp, end = 16.dp),
+                        steps = 5
+                    )
+                }
+            //}else {
+                //Box() {
+
+                //}
+            //}
         }
     }
 }
