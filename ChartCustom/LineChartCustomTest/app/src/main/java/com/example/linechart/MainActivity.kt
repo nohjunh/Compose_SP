2a3acf0d6254761c920e8edbc94ed5be24fa9c13
nohjunh <nsh042777@naver.com>
2023-04-05 14:04:15 +0900

    LineChartCustom

--- ChartCustom/LineChartCustomTest/app/src/main/java/com/example/linechart/MainActivity.kt
+++ ChartCustom/LineChartCustomTest/app/src/main/java/com/example/linechart/MainActivity.kt
@@ -11,12 +11,14 @@ import android.graphics.Paint
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Divider
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlin.math.round
@@ -32,50 +34,27 @@ class MainActivity : ComponentActivity() {
             LineChartTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val data = listOf(
-                        Pair(6, 111.45),
-                        Pair(7, 111.0),
-                        Pair(8, 113.45),
-                        Pair(9, 112.25),
-                        Pair(10, 116.45),
-                        Pair(11, 113.35),
-                        Pair(12, 118.65),
-                        Pair(13, 110.15),
-                        Pair(14, 113.05),
-                        Pair(15, 114.25),
-                        Pair(16, 116.35),
-                        Pair(17, 117.45),
-                        Pair(18, 112.65),
-                        Pair(19, 115.45),
-                        Pair(20, 111.85)
-                    )
-                    Column(
+                    Box(
                         modifier = Modifier
-                            .fillMaxSize()
-                            .background(Color.Black)
-                            .padding(16.dp),
-                        verticalArrangement = Arrangement.Center
+                            .size(300.dp) // 크기 제한
+                            .clip(RoundedCornerShape(16.dp)) // 둥근 테두리 적용
+                            .background(colorResource(id = R.color.chart_box_color))
+                            .padding(5.dp)
                     ) {
-                        LineChart(
-                            data = data,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(300.dp)
-                                .align(CenterHorizontally)
-                        )
-                        Spacer(modifier = Modifier.height(40.dp))
-                        Divider()
-                        Spacer(modifier = Modifier.height(40.dp))
-                        QuadLineChart(
-                            data = data,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(300.dp)
-                                .align(CenterHorizontally)
-                        )
+                        Column(
+                            modifier = Modifier.padding(15.dp),
+                            verticalArrangement = Arrangement.Center,
+                        ) {
+                            Text(
+                                text = "CPU 사용률(%)",
+                                fontSize = 25.sp,
+                                color = Color.White
+                            )
+                            Spacer(modifier = Modifier.height(50.dp))
+                            LineChartScreen()
+                        }
                     }
                 }
             }
@@ -84,9 +63,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun LineChartScreen() {
+    val data = listOf(
+        Pair(1, 111.45),
+        Pair(2, 111.0),
+        Pair(3, 113.45),
+        Pair(4, 112.25),
+        Pair(5, 116.45),
+        Pair(6, 118.65),
+        Pair(7, 110.15),
+        Pair(8, 113.05),
+        Pair(9, 114.25),
+        Pair(10, 111.85),
+        Pair(11, 110.85)
+    )
+    LineChart(
+        data = data,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(300.dp)
+    )
+}
+
+@Composable
 fun LineChart(
+    modifier: Modifier = Modifier,
     data: List<Pair<Int, Double>> = emptyList(),
-    modifier: Modifier = Modifier
 ) {
     val spacing = 100f
     val graphColor = Color.Cyan
@@ -105,11 +107,11 @@ fun LineChart(
 
     Canvas(modifier = modifier) {
         val spacePerHour = (size.width - spacing) / data.size
-        (data.indices step 2).forEach { i ->
+        (data.indices step 5).forEach { i -> // Step 5로 해서 간격 여유두기
             val hour = data[i].first
             drawContext.canvas.nativeCanvas.apply {
                 drawText(
-                    hour.toString(),
+                    "${hour.toString()}:00",
                     spacing + i * spacePerHour,
                     size.height,
                     textPaint
@@ -148,104 +150,6 @@ fun LineChart(
             color = graphColor,
             style = Stroke(
                 width = 2.dp.toPx(),
-                cap = StrokeCap.Round
-            )
-        )
-
-        val fillPath = android.graphics.Path(strokePath.asAndroidPath()).asComposePath().apply {
-            lineTo(size.width - spacePerHour, size.height - spacing)
-            lineTo(spacing, size.height - spacing)
-            close()
-        }
-
-        drawPath(
-            path = fillPath,
-            brush = Brush.verticalGradient(
-                colors = listOf(
-                    transparentGraphColor,
-                    Color.Transparent
-                ),
-                endY = size.height - spacing
-            )
-        )
-
-    }
-}
-
-@Composable
-fun QuadLineChart(
-    data: List<Pair<Int, Double>> = emptyList(),
-    modifier: Modifier = Modifier
-) {
-    val spacing = 100f
-    val graphColor = Color.Red
-    val transparentGraphColor = remember { graphColor.copy(alpha = 0.5f) }
-    val upperValue = remember { (data.maxOfOrNull { it.second }?.plus(1))?.roundToInt() ?: 0 }
-    val lowerValue = remember { (data.minOfOrNull { it.second }?.toInt() ?: 0) }
-    val density = LocalDensity.current
-
-    val textPaint = remember(density) {
-        Paint().apply {
-            color = android.graphics.Color.WHITE
-            textAlign = Paint.Align.CENTER
-            textSize = density.run { 12.sp.toPx() }
-        }
-    }
-
-    Canvas(modifier = modifier) {
-        val spacePerHour = (size.width - spacing) / data.size
-        (data.indices step 2).forEach { i ->
-            val hour = data[i].first
-            drawContext.canvas.nativeCanvas.apply {
-                drawText(
-                    hour.toString(),
-                    spacing + i * spacePerHour,
-                    size.height,
-                    textPaint
-                )
-            }
-        }
-
-        val priceStep = (upperValue - lowerValue) / 5f
-        (0..4).forEach { i ->
-            drawContext.canvas.nativeCanvas.apply {
-                drawText(
-                    round(lowerValue + priceStep * i).toString(),
-                    30f,
-                    size.height - spacing - i * size.height / 5f,
-                    textPaint
-                )
-            }
-        }
-
-        var medX: Float
-        var medY: Float
-        val strokePath = Path().apply {
-            val height = size.height
-            data.indices.forEach { i ->
-                val nextInfo = data.getOrNull(i + 1) ?: data.last()
-                val firstRatio = (data[i].second - lowerValue) / (upperValue - lowerValue)
-                val secondRatio = (nextInfo.second - lowerValue) / (upperValue - lowerValue)
-
-                val x1 = spacing + i * spacePerHour
-                val y1 = height - spacing - (firstRatio * height).toFloat()
-                val x2 = spacing + (i + 1) * spacePerHour
-                val y2 = height - spacing - (secondRatio * height).toFloat()
-                if (i == 0) {
-                    moveTo(x1, y1)
-                } else {
-                    medX = (x1 + x2) / 2f
-                    medY = (y1 + y2) / 2f
-                    quadraticBezierTo(x1 = x1, y1 = y1, x2 = medX, y2 = medY)
-                }
-            }
-        }
-
-        drawPath(
-            path = strokePath,
-            color = Color.Red,
-            style = Stroke(
-                width = 3.dp.toPx(),
                 cap = StrokeCap.Round
             )
         )
